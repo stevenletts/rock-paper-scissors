2a014fa254900b055332126f34090d82f0434d76
Steven <s.spencerletts@gmail.com>
2023-02-01 12:54:54 +0000

    Logic and interaction in console completed for rock paper scissors Need to add check for user input to be within the defined choices

---
+++ script.js
@@ -0,0 +1,43 @@
+function getComputerChoice() {
+    const selection = ["Rock", "Paper", "Scissors"];
+    const random = Math.floor(Math.random() * selection.length);
+    console.log(selection[random]);
+    return selection[random];
+}
+
+function playRound(playerSelection, computerSelection){
+    const player = playerSelection.toLowerCase();
+    const computer = computerSelection.toLowerCase();
+
+    if (player === computer){
+        return "Draw";
+    } else if (player === "rock"){
+        if (computer === "scissors"){
+            return `Player wins, ${player} beats ${computer}`;
+        } else {
+            return `Computer wins ${computer} beats ${player}`;
+        }
+    } else if (player === "paper"){
+        if (computer === "rock"){
+            return `Player wins, ${player} beats ${computer}`;
+        } else {
+            return `Computer wins ${computer} beats ${player}`;
+        }
+    } else if (player === "scissors"){
+        if (computer === "rock"){
+            return `Player wins, ${player} beats ${computer}`;
+        } else {
+            return `Computer wins ${computer} beats ${player}`;
+        }
+    } else {
+        console.log("logic error");
+    }
+}
+
+function game(){
+    for (let i = 0; i < 5; i++){
+        const choice = String(prompt("Rock, Paper or Scissors"));
+        console.log(typeof(choice))
+        console.log(playRound(choice, getComputerChoice()));
+    }
+}
